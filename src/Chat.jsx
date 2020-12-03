547800e4fcba712853fad07b991a1eaac437c31e
shehan82 <shehansandeepa82@gmail.com>
2020-12-03 16:19:17 +0530

    create chat header

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Avatar, IconButton ,Input} from '@material-ui/core';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { SearchOutlined } from '@material-ui/icons';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
 import './Chat.css'
 
 function Chat() {
     return (
         <div className="chat">
-            <h1>hello i am from chat</h1>
+            <div className="chat__header">
+                {/* 1 */}
+                <Avatar/>
+
+                {/* 2 */}
+                <div className="chat__headerInfo">
+                    <h3>chat room 1</h3>
+                    <p>this is last seen</p>
+                </div>
+                
+                {/* 3 */}
+                <div className="chat__headerRight">
+               
+                    <IconButton>
+                        <SearchOutlined/>
+                    </IconButton>
+                    <IconButton>
+                        <AttachFileIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon/>
+                    </IconButton>
+                </div>
+            </div>
         </div>
     )
 }
