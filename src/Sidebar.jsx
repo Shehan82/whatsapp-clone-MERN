076597421f8db94cde42a173eaa31eb562835382
shehan82 <shehansandeepa82@gmail.com>
2020-12-03 15:03:17 +0530

    create sidebar chat boxes

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Sidebar.css';
 import ChatIcon from '@material-ui/icons/Chat';
+import SidebarChat from './SidebarChat';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { Avatar, IconButton ,Input} from '@material-ui/core';
@@ -8,6 +9,7 @@ import { SearchOutlined } from '@material-ui/icons';
 
 function Sidebar(props) {
     return (
+
         <div className="sidebar">
             <div className="sidebar__header">
                 <Avatar src="" />
@@ -23,12 +25,21 @@ function Sidebar(props) {
                     </IconButton>
                 </div>
             </div>
+
             <div className="sidebar__search">
                 <div className="sidebar__searchContainer">
                     <SearchOutlined/>
                     <input className="sidebar__searchInput" type="text"  placeholder="Search or start new chat"/>
                 </div>
-       
+            </div>
+
+            <div className="sidebar__chat">
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
+                
             </div>
         </div>
     )
--- src/SidebarChat.jsx
+++ src/SidebarChat.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import './SidebarChat';
+import './SidebarChat.css';
+import { Avatar, IconButton ,Input} from '@material-ui/core';
 
 function SidebarChat() {
     return (
-        <div>
-            
+        <div className="sidebarChat">
+            <Avatar/>
+            <div className="sidebarChat__info">
+                    <h4>chat room</h4>
+                    <p>this is the last massege</p>
+            </div>
         </div>
     )
 }
