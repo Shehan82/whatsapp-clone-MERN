aa0d08ff389adcb53040b6153a5a7ff2a5522ef0
shehan82 <shehansandeepa82@gmail.com>
2020-12-03 10:08:45 +0530

    create two comp, chat and sidebar with relavent css files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCNhasepiaPrgmY9tzFdTe0FlDVzi9q0r4",
+    authDomain: "whatsapp-clone-mern-4df7c.firebaseapp.com",
+    projectId: "whatsapp-clone-mern-4df7c",
+    storageBucket: "whatsapp-clone-mern-4df7c.appspot.com",
+    messagingSenderId: "383542399854",
+    appId: "1:383542399854:web:9db99a2d79211b725fa77f",
+    measurementId: "G-SKK0124Z2F"
+  };
