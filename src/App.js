0ec60953c2dae2b6cb52171d51bb6d7e2094df37
shehan82 <shehansandeepa82@gmail.com>
2020-12-04 21:10:56 +0530

    add pusher to frontend

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import Chat from './Chat';
 import Sidebar from './Sidebar';
 
 function App() {
+
+  useEffect(() => {
+    var pusher = new Pusher('bae7fff4f6e77102979d', {
+      cluster: 'eu'
+    });
+
+    var channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(data) {
+      alert(JSON.stringify(data));
+    });
+
+  }, [])
+
   return (
     <div className="app">
       <div className="app__body">
